43cfa7ad87a53c9f8b919d56ff89328628034e11
Ian C <insung1@ualberta.ca>
2022-06-07 09:45:11 -0600

    Initial files
    Initial files

---
+++ src/editor/action/ActionList.py
@@ -0,0 +1,8 @@
+# This Python file uses the following encoding: utf-8
+from PySide6 import QtCore
+from PySide6 import QtQuick
+
+
+class ActionList(QtQuick.QQuickItem):
+    def __init__(self):
+        pass
