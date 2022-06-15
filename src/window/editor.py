b4551981affb5eff66cf586ddf210b2fd8d50264
TheLittleDoctor <36447611+TheLittleDoc@users.noreply.github.com>
2022-06-15 11:58:16 -0400

    Utilizing resourcePath()

--- src/window/editor.py
+++ src/window/editor.py
@@ -2,9 +2,11 @@ import os
 
 from PyQt6 import QtWidgets, uic
 from pathlib import Path
+from gm_resources import *
 
 class Editor(QtWidgets.QMainWindow):
     def __init__(self, *args):
         super().__init__(*args) # Call the inherited classes __init__ method
         path = os.fspath(Path(__file__).resolve().parent / "ui/editor.ui")
+        path = resourcePath("src\\window\\ui\\editor.ui")
         uic.loadUi(path, self) # Load the .ui file
--- src/window/mainwindow.py
+++ src/window/mainwindow.py
@@ -4,6 +4,7 @@ from PyQt6 import QtWidgets, uic
 from pathlib import Path
 from .startmenu import StartMenu
 from .editor import Editor
+from gm_resources import * # Importing my PyInstaller resource manager
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args):
@@ -13,6 +14,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def initUI(self):
         path = os.fspath(Path(__file__).resolve().parent / "ui/mainwindow.ui") # This is very clunky, should change this.
+        path = resourcePath("src\\window\\ui\\mainwindow.ui") # replaced complicated path logic with resourcePath()
         uic.loadUi(path, self) # Load the .ui file
         self.show() # Show the GUI
 
