5a9ce1c67ff6e02804e82d0b924930ab9c1cb4d4
TheLittleDoctor <36447611+TheLittleDoc@users.noreply.github.com>
2022-06-18 15:51:52 -0400

    Fixed dialogs and created our own MessageBoxes

--- src/gm_resources.py
+++ src/gm_resources.py
@@ -6,14 +6,35 @@ import webbrowser
 
 f = {}
 
-def externalLink(self, link):
+class GMessageBox:
+    """A class defining the types of message boxes we'll be using. I'm dumb and don't know how else to do this, but it didn't look like QMessageBox included the things I was expecting it to lmao"""
+    def __init__(self,title,message,type):
+        self.title = title
+        self.message = message
+        self.type = type
+        self.popUp = QMessageBox()
+        self.popUp.setWindowTitle(title)
+        self.popUp.setText(message)
+        if "Ask" in type:
+            self.popUp.setIcon(QMessageBox.Icon.Question)
+            if type == "AskYesNo":
+                self.popUp.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+            elif type == "AskYesAllNo":
+                self.popUp.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.YesToAll | QMessageBox.StandardButton.No)
+            else:
+                raise Exception("Unknown MessageBox type. Currently available types are AskYesNo, AskYesAllNo, and Info")
+        elif type == "Info":
+            self.popUp.setIcon(QMessageBox.Icon.Information)
+            self.popUp.setStandardButtons(QMessageBox.StandardButton.Ok)
+        else:
+            raise Exception("Unknown MessageBox type. Currently available types are AskYesNo, AskYesAllNo, and Info")
+    def exec(self):
+        return self.popUp.exec()
+
+def externalLink(link):
     """ Shows confirmation dialog before opening a link in the default or most recently used browser """
-    asklink = QMessageBox()
-    asklink.setWindowTitle("Open link")
-    asklink.setText("GameMaster is opening \n\"%s\" \nin your default browser.\n\nDo you want to continue?" % link)
-    asklink.setStandardButtons(QMessageBox.StandardButton.Cancel | QMessageBox.StandardButton.Yes)
-    asklink.setIcon(QMessageBox.Icon.Question)
-    if asklink.exec() == QMessageBox.StandardButton.Ok:
+    asklink = GMessageBox("Open link","GameMaster is opening \n\"%s\" \nin your default browser.\n\nDo you want to continue?" % link,"AskYesNo")
+    if asklink.exec() == QMessageBox.StandardButton.Yes:
         webbrowser.open(link)
     else:
         None
@@ -29,20 +50,21 @@ def resourcePath(relative_path):
     return os.path.join(base_path, relative_path)
 
 def retrieveFile(url, name):
-    """Not working. Fix dialogs before use."""
     if not "http" in url:
-        qm.ABOUT(title='Invalid URL',message='URL must start with "http"', icon='warning')
+        fileWarning = GMessageBox(title='Invalid URL',message='URL must start with "http"', type="Info")
+        fileWarning.exec()
+        pass
     else:
         file = requests.get(url)
         f[name] = ""
         for line in file:
-            f[name] += line # Removed decode as it's already been decoded.
-    return f[name]
+            f[name] += line.decode() # Removed decode as it's already been decoded.
+        return f[name]
 
 def downloadFile(url, name):
-    """Not working. Fix dialogs before use."""
     if not "http" in url:
-        qm.ABOUT(title='Invalid URL',message='URL must start with "http"', icon='warning')
+        fileWarning = GMessageBox(title='Invalid URL',message='URL must start with "http"', type="Info")
+        fileWarning.exec()
     else:
         file = requests.get(url)
         with open(name, "w") as f:
