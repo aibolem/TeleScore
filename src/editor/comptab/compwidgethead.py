104b5ffc3af791ae87e000a5f397cb4aa5dfc315
Ian C <insung1@ualberta.ca>
2022-07-22 14:49:49 -0600

    Changed

--- src/editor/comptab/compwidgethead.py
+++ src/editor/comptab/compwidgethead.py
@@ -15,4 +15,5 @@ class CompWidgetHead(QStandardItem):
         self.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
         self.setText(text) # Very bad way of centering it, change it at some point
         self.setFont(QFont("Open Sans Bold", 13))
+        self.setEditable(False)
         self.setBackground(QColor(200, 200, 200))
